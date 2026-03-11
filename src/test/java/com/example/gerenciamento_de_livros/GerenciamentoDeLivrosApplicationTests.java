98eb1bb5d35c46467b852a74a9632e5cbbdbcba7
Gustavo Guimarães <gustavodossantosguimaraes93@gmail.com>
2026-03-11 19:42:29 -0300

    first commit

---
+++ src/test/java/com/example/gerenciamento_de_livros/GerenciamentoDeLivrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gerenciamento_de_livros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoDeLivrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
